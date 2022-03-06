b511b7c6d4059a85c2c022539d7fa7744b9ddd22
Ashish sharda <shardaashish097@gmail.com>
2022-03-07 02:10:50 +0530

    code file added

---
+++ script.js
@@ -0,0 +1,55 @@
+const quoteContainer = document.getElementById("quote-container")
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show new quotes
+
+function newQuote(){
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    // check length of quote and add class
+    if (quote.text.length > 120) {
+        quoteText.classList.add("long-quote");
+    }
+    else(quoteText.classList.remove("long-quote"))
+    
+    quoteText.textContent = quote.text
+
+// check if author is blank
+    if (!quote.author) {
+        authorText.textContent = "Anonymous";
+    }
+    else {
+
+        authorText.textContent = quote.author
+    }
+}
+//  Get quotes from api
+
+async function getQuotes() {
+    const apiUrl = "https://type.fit/api/quotes";
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        
+    }
+}
+
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl,'_blank');
+}
+
+twitterBtn.addEventListener('click',tweetQuote)
+newQuoteBtn.addEventListener('click',newQuote)
+
+getQuotes()
